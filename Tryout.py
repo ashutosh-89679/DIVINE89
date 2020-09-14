b2f58a00ff80e0cd4f0de46f6419fd2434637c1c
DIVIПΣ89 <ashutosh89679@gmail.com>
2020-09-14 15:06:35 +0530

    Update Tryout.py

--- Tryout.py
+++ Tryout.py
@@ -1,3 +1,11 @@
 # DIVINE89
 Code accuration and storing 
-print("hello")
+command =""
+while command.lower() != "quit":
+    command = input(">")
+    if command == "its is going good":
+        print("be free")
+elif command =  "it is not going good"
+        print(" go and contact them")
+    else:
+        print(" i dont understand that")
